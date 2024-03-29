94770e82eb0b558edfc644f8663c2e3b956f07eb
afdaldev <46302343+afdaldev@users.noreply.github.com>
2024-03-30 01:40:26 +0700

    feat: improve main.dart with W3MService connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:web3dart_example/blocs/staking_bloc.dart';
+import 'package:web3dart_example/staking/blocs/staking_bloc.dart';
+import 'package:web3dart_example/staking/views/staking_view.dart';
+import 'package:web3dart_example/web3/blocs/wallet_connect_bloc.dart';
+import 'package:web3modal_flutter/web3modal_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +14,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<StakingBloc>(
-      create: (context) => StakingBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<WalletConnectBloc>(
+          create: (context) => WalletConnectBloc(),
+        ),
+        BlocProvider<StakingBloc>(
+          create: (context) => StakingBloc(
+            walletConnect: BlocProvider.of<WalletConnectBloc>(context),
+          ),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -32,34 +44,71 @@ class Web3DartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final w3mService = context.watch<WalletConnectBloc>().w3mService;
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: BlocBuilder<StakingBloc, StakingState>(
+        child: BlocBuilder<WalletConnectBloc, WalletConnectState>(
           builder: (context, state) {
-            if (state.isLoading) {
-              return const SizedBox(
-                height: 100,
-                width: 100,
-                child: CircularProgressIndicator(),
-              );
-            } else if (!state.isLoading && state.staking != null) {
-              return Column(
-                children: [
-                  Text('Max Cap : ${state.staking?.maxCap}'),
-                  Text('Locked Period : ${state.staking?.lockedPeriod}'),
-                  Text('Total Staked : ${state.staking?.totalStaked}'),
-                  Text('APY : ${state.staking?.apy}'),
-                ],
-              );
+            if (state.isConnect) {
+              if (w3mService.selectedChain?.chainId == '56' ||
+                  w3mService.selectedChain?.chainId == '97') {
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const StakingView(),
+                      W3MNetworkSelectButton(
+                        service: w3mService,
+                        size: BaseButtonSize.small,
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.wallet_rounded,
+                      size: 48.0,
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Select Binance Smart Chain Test Net',
+                    ),
+                    const SizedBox(height: 8.0),
+                    W3MNetworkSelectButton(
+                      service: w3mService,
+                      size: BaseButtonSize.small,
+                    ),
+                  ],
+                );
+              }
             } else {
-              return ElevatedButton(
-                onPressed: () => context
-                    .read<StakingBloc>()
-                    .add(const StakingEvent.getStaking()),
-                child: const Text(
-                  'Fetch',
-                ),
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.wallet_rounded,
+                    size: 48.0,
+                  ),
+                  const SizedBox(height: 16.0),
+                  Visibility(
+                    visible: !state.isConnect,
+                    child: W3MNetworkSelectButton(
+                      service: w3mService,
+                      size: BaseButtonSize.small,
+                    ),
+                  ),
+                  Visibility(
+                    visible: !state.isConnect,
+                    child: W3MConnectWalletButton(
+                      service: w3mService,
+                      size: BaseButtonSize.small,
+                    ),
+                  ),
+                ],
               );
             }
           },
